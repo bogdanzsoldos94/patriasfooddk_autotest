b950029ecb208353282b532a981d360b82772e4d
bogdanzsoldos94 <bogdanzsoldos94@gmail.com>
2024-12-01 17:50:40 +0100

    Added new properties in POM and a regressionRun as a runSuite

--- src/test/java/data/LoginDP.java
+++ src/test/java/data/LoginDP.java
@@ -1,4 +1,16 @@
 package data;
 
+import org.testng.annotations.DataProvider;
+
 public class LoginDP {
+
+    @DataProvider
+    public Object[][] loginDataProvider() {
+        return new Object[][]{
+//          username, password, browser
+                {"zebra", "zebrapassword", "edge"},
+                {"dingo", "dingopassword", "chrome"},
+                {"camel", "camelpassword", "edge"},
+        };
+    }
 }
